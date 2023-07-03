76dd4cc5e66d7b466bb47e83cd47a5d7ef00c663
unknown <you@example.com>
2023-07-03 13:42:19 +0100

    OC:Added STUFF

---
+++ script2.js
@@ -0,0 +1,17 @@
+let list = ["random", "random", "random"]
+console.log(list)
+
+console.log(list.length)
+
+list.pop()
+console.log(list)
+
+list.push("new word")
+console.log(list)
+
+list[0]= "swap"
+console.log(list)  
+
+list.reverse()
+console.log(list)
+
